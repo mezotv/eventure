0e0619fde319641dfb01fcc86f5a7eaa29809786
maxximum <64035335+xMaxximum@users.noreply.github.com>
2025-02-28 14:27:16 +0100

    add functionality to login pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { LoginComponent as OldLogin } from './auth/components/login/login.component';
-import { RegisterComponent as Oldregister } from './auth/components/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -9,8 +7,6 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'oldregister', component: Oldregister },
-  { path: 'oldlogin', component: OldLogin },
   //   {
   //     path: 'dashboard',
   //     loadComponent: () =>
