ff6e1bcb6a85e781c4101ed18a3a6eb5254b9e08
maxximum <64035335+xMaxximum@users.noreply.github.com>
2025-02-28 11:16:09 +0100

    fix merge conflict

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent as LoginComponentOld } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  //   { path: '' },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponentOld },
   { path: 'register', component: RegisterComponent },
   //   {
@@ -14,9 +16,4 @@ export const routes: Routes = [
   //       ),
   //     canActivate: [authGuard],
   //   },
-];
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
 ];
