884c62eaa91facbd9c25bf2c0fbc28d9a63ec39b
Dominik Koch <dominik@koch-bautechnik.de>
2025-03-05 14:48:24 +0100

    feat: shows full name with dropdown

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../auth/services/auth.service';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatButtonModule, RouterModule],
+  imports: [MatButtonModule, RouterModule, MatMenuModule, MatDividerModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
