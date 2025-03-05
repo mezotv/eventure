4827edd64b4e9c5b5e27017beab384f919847042
Dominik Koch <dominik@koch-bautechnik.de>
2025-03-05 15:13:35 +0100

    fix: remove legal notice

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterModule],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
